21ef648bdd0d4bb7e98431012dbbed6e984a5659
William-Yulio <william.yulio331@gmail.com>
2022-07-21 14:08:11 +0700

    Initial Commit

---
+++ NC2 William Yulio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NC2 William Yulio
+//
+//  Created by William Yulio on 21/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NC2 William Yulio/NC2_William_YulioApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC2_William_YulioApp.swift
+//  NC2 William Yulio
+//
+//  Created by William Yulio on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct NC2_William_YulioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
